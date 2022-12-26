3a6d504ff2b3ebfd706edfbf6d6bc37ba84e2e46
Shohzod <xorozovshohzod@gmail.com>
2022-12-26 08:40:28 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,77 +1,10 @@
-import { Container } from "./component/styled/Container.styled";
-import {
-  Navbar,
-  FlexBox,
-  LinkWrapp,
-  Payment,
-  ViseCard,
-} from "./component/styled/Navbar.styled";
-import { NavLink } from "react-router-dom";
-import visa from "./images/visa.png";
-import master from "./images/master.png";
-import bitcoin from "./images/bitcoin.png";
 function App() {
-  const links = [
-    {
-      page: "Home",
-      url: "/",
-    },
-    {
-      page: "Cars",
-      url: "/car",
-    },
-    {
-      page: "Countries",
-      url: "/country",
-    },
-    {
-      page: "Brands",
-      url: "/brand",
-    },
-    {
-      page: "Services",
-      url: "/sevice",
-    },
-    {
-      page: "About Us",
-      url: "/about",
-    },
-    {
-      page: "FAQ",
-      url: "/faq",
-    },
-    {
-      page: "Contacts",
-      url: "/contact",
-    },
-  ];
-
   return (
-    <Navbar bg="#000">
-      <Container>
-        <FlexBox>
-          <LinkWrapp>
-            {links.map((item, i) => {
-              return (
-                <NavLink
-                  to={item.url}
-                  key={i}
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  {item.page}
-                </NavLink>
-              );
-            })}
-          </LinkWrapp>
-          <Payment>
-            <h3>Payment methods</h3>
-            <ViseCard src={visa} alt="visa" />
-            <ViseCard src={master} alt="visa" />
-            <ViseCard src={bitcoin} alt="visa" />
-          </Payment>
-        </FlexBox>
-      </Container>
-    </Navbar>
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
